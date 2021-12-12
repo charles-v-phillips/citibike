5ac59ddd9c7f7d0f70cb3411b2dc5b577ba3a0dc
charles phillips <charlesvphillips@gmail.com>
2021-12-12 17:29:18 -0500

    added usage patterns to app

--- dash_app/eda_tab.py
+++ dash_app/eda_tab.py
@@ -3,6 +3,8 @@ from dash import dcc, html
 import pickle
 import json
 import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
 
 
 pop_data = pickle.load(open('./data/pop_data.pkl', 'rb')) # population density data frame
@@ -29,6 +31,28 @@ transit_locations = px.scatter_mapbox(dock_train_bus_df,
                   center = dict(lat = 40.76421, lon = -73.95623)
                  )
 
+usage_patterns = pd.read_csv('data/usage_patterns.csv')
+usage_trend_plot = go.Figure(go.Heatmap(
+                z = usage_patterns['num_rides'],
+                x = usage_patterns['hour_of_day'],
+                y = usage_patterns['day_of_week'],
+                colorscale=[[0, 'white'], [1, 'blue']]))
+usage_trend_plot.update_layout(
+    yaxis = dict(
+        tickmode = 'array',
+        tickvals = [1, 2, 3, 4, 5, 6,7],
+        ticktext = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday']
+    ),
+    xaxis = dict(
+        tickmode = 'array',
+        tickvals = list(range(24)),
+        ticktext = ['12 AM','','','','4 AM','','','','8 AM','','','','12 PM','','','','4 PM','','','','8 PM','','','11 PM']
+        ),
+    font = dict(size = 16),
+    title = dict(text='WILL THINK OF SOMETHING')
+
+)
+
 eda_tab = dcc.Tab(label='EDA',
                   value='eda',
                   children = [
@@ -43,5 +67,7 @@ eda_tab = dcc.Tab(label='EDA',
 
                         dcc.Graph(id='pop_density' ,figure=pop_density),
 
-                        dcc.Graph(id = 'transit_locations', figure = transit_locations)
+                        dcc.Graph(id = 'transit_locations', figure = transit_locations),
+
+                        dcc.Graph(id='usage_pattern',figure=usage_trend_plot)
 ])
