b676f4950ba3d9f69c2095ca085cd301b06f3069
Hugh Goode <hjgoode3@gmail.com>
2021-12-12 15:54:03 -0500

    dash app about us tab

--- dash_app/about_us_tab.py
+++ dash_app/about_us_tab.py
@@ -1,3 +1,23 @@
 from dash import dcc, html
+from PIL import Image
+
+hugh_summary = 'sdklfjaslkfjadslkfjasdl;kjflakdsj'
+hugh_photo = Image.open('./data/image/hugh.jpg')
+
 about_us_tab = dcc.Tab(label = 'About Us',
-                    value='about_us')
+                    value='about_us',
+                    children = [
+                    html.H2("Team Information"),
+                    html.H3("Hugh"),
+                    html.Img(id = 'photo',
+                        src = hugh_photo,
+                        style = {'width': 300, 'height': 300}),
+                    dcc.Textarea(id = 'bio',
+                        value = hugh_summary,
+                        style={'width': '100%'}),
+                    html.A("LinkedIn",
+                        href = 'https://www.linkedin.com'),
+                    html.H3("Charles"),
+                    html.H3("Jung"),
+                    html.H3("Robert"),
+                        ])
--- dash_app/eda_tab.py
+++ dash_app/eda_tab.py
@@ -1,4 +1,33 @@
+import dash
 from dash import dcc, html
+import pickle
+import json
+import plotly.express as px
+
+
+pop_data = pickle.load(open('./data/pop_data.pkl', 'rb')) # population density data frame
+
+with open('./data/N_Areas.geojson', 'r') as j: # shape file for neighborhood boundaries, needed for population density plot
+    boundaries = json.loads(j.read())
+
+dock_train_bus_df = pickle.load(open('./data/dock_train_bus_df.pkl', 'rb')) # dataframe with dock locations, bus stop location, and subway station locations
+
+pop_density =px.choropleth_mapbox(pop_data,
+                          geojson=boundaries,
+                          locations = 'Neighborhood Tabulation Area Code (NTA Code)',
+                           featureidkey="properties.ntacode",
+                          color = 'Population Density (per Sq. Mi.)',
+                           center={"lat": 40.77, "lon": -73.79},
+                          mapbox_style="carto-positron")
+transit_locations = px.scatter_mapbox(dock_train_bus_df,
+                lat = 'latitude',
+                lon = 'longitude',
+                 color = 'type',
+                 mapbox_style = 'carto-positron',
+                  #color_continuous_scale=['pink','red','orange', 'yellow' 'green', 'blue','purple' ,'grey','black'],
+                  zoom = 10,
+                  center = dict(lat = 40.76421, lon = -73.95623)
+                 )
 
 eda_tab = dcc.Tab(label='EDA',
                   value='eda',
@@ -10,6 +39,9 @@ eda_tab = dcc.Tab(label='EDA',
                             max=9,
                             value=1,
                         ),
-                        dcc.Graph(id='rollout_map')
-])
+                        dcc.Graph(id='rollout_map'),
 
+                        dcc.Graph(id='pop_density' ,figure=pop_density),
+
+                        dcc.Graph(id = 'transit_locations', figure = transit_locations)
+])
