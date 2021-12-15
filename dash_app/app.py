d77fe91ccd8e25d1edfecaa1ef10d12f047b08d7
charles phillips <charlesvphillips@gmail.com>
2021-12-15 17:05:31 -0500

    Fixed bug in dynamic Rollout Chart

--- dash_app/app.py
+++ dash_app/app.py
@@ -12,6 +12,7 @@ import layout
 
 
 rollout_data = pd.read_csv('./data/rollout_clusters.csv')
+rollout_data['rollout_cluster'] = rollout_data['rollout_cluster'].astype(str)
 
 
 #-------------------- LAYOUT ------------------------------
@@ -31,15 +32,15 @@ app.layout = layout.layout
 )
 def update_plot(rollout):
     copy = rollout_data.copy()
-    copy = copy[copy['rollout_cluster'] <= rollout]
+    copy = copy[copy['rollout_cluster'].isin([str(i) for i in range(1,rollout+1)])]
 
     map = px.scatter_mapbox(copy,
                             lat='latitude',
                             lon='longitude',
                             color='rollout_cluster',
                             mapbox_style='carto-positron',
-                            color_continuous_scale=['pink', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'grey',
-                                                    'black','white'][1:rollout+1],
+                            # color_continuous_scale=['pink', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'grey',
+                                                    # 'black','white'],
 
                             zoom=10,
                             center=dict(lat=40.76421, lon=-73.95623)
