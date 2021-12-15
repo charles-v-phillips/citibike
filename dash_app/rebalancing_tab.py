581b94c3ed96049086ad14c418fbb951d7096e04
charles phillips <charlesvphillips@gmail.com>
2021-12-14 19:03:19 -0500

    Added cluster visuals to Rebalancing Tab

--- dash_app/rebalancing_tab.py
+++ dash_app/rebalancing_tab.py
@@ -4,12 +4,21 @@ import pandas as pd
 import numpy as np
 import dash_deck
 import json
+from PIL import Image
 from secrets import mapbox_key
 with open('./data/weekday_rebalancing.json', 'r') as js: # shape file for neighborhood boundaries, needed for population density plot
     weekday_rebalanced_plot = json.loads(js.read())
 
 with open('./data/weekend_rebalancing.json', 'r') as js: # shape file for neighborhood boundaries, needed for population density plot
     weekend_rebalanced_plot = json.loads(js.read())
+
+cluster0 = Image.open('./data/weekday_cluster_images/weekday_cluster_0.png')
+cluster1 = Image.open('./data/weekday_cluster_images/weekday_cluster_1.png')
+cluster2 = Image.open('./data/weekday_cluster_images/weekday_cluster_2.png')
+cluster3 = Image.open('./data/weekday_cluster_images/weekday_cluster_3.png')
+cluster4 = Image.open('./data/weekday_cluster_images/weekday_cluster_4.png')
+cluster5 = Image.open('./data/weekday_cluster_images/weekday_cluster_5.png')
+cluster6 = Image.open('./data/weekday_cluster_images/weekday_cluster_6.png')
 
 
 
@@ -39,13 +48,22 @@ rebalancing_tab = dcc.Tab(label='Rebalancing',
                                          style={'width': '60%','height' : '100%', 'display': 'inline-block'}),
 
                                 html.Div(children = [html.H2('Weekend Rebalancing'),'text'],
-                                         style={'width': '30%', 'display': 'inline-block','vertical-align': 'top','margin-left': '9%'})],style = {'height' : '45vh','margin-top':'10px'})
-                                   ]
-                          )
+                                         style={'width': '30%', 'display': 'inline-block','vertical-align': 'top','margin-left': '9%'})],style = {'height' : '45vh','margin-top':'10px'}),
+                          html.H1('Clustering Stations'),
+                          html.Div(children = [ html.Div(children = [html.Img(id = 'c1',src = cluster0,style = {"height": "30vh","width": "auto"}),
+                                                        html.Img(id = 'c2',src = cluster1,style = {"height": "30vh","width": "auto"}),
+                                                        html.Img(id = 'c3',src = cluster2,style = {"height": "30vh", "width": "auto"}),
+                                                        html.Img(id = 'c4',src = cluster3,style = {"height": "30vh", "width": "auto"}),
+                                                        html.Img(id = 'c5',src = cluster4,style = {"height": "30vh", "width": "auto"}),
+                                                        html.Img(id = 'c6',src = cluster5,style = {"height": "30vh", "width": "auto"}),
+                                                        html.Img(id = 'c7',src = cluster6,style = {"height": "30vh", "width": "auto"})],
+                                                    style={'width': '60%','height' : '100%', 'display': 'inline-block'}),
+                                                html.Div(children = [html.H1('DUMMY HEader'), 'dummy_text'],
+                                                         style={'width': '30%','margin-left' : '9%','height' : '100%', 'display': 'inline-block','vertical-align': 'top'})
 
-                          # html.H3('Weekday Rebalancing'),
-                          # dash_deck.DeckGL(weekday_rebalanced_plot,
-                          #           style={"width": "50vw", "height": "50vh","position": 'relative'},
-                          #           id='deck-gl',
-                          #           tooltip=TOOLTIP_TEXT,
-                          #           mapboxKey=mapbox_key)])
+
+
+                                                ])
+                                   ]
+
+                          )
