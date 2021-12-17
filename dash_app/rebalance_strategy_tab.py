2dde9be3576c12ad232e4b3a894e8af91213551e
charles phillips <charlesvphillips@gmail.com>
2021-12-16 22:09:54 -0500

    Formatting App

--- dash_app/rebalance_strategy_tab.py
+++ dash_app/rebalance_strategy_tab.py
@@ -1,8 +1,12 @@
 from dash import dcc, html
 
-rebalance_intro = "FUCKKKK"
 
-rebalance_strategy_tab = dcc.Tab(label = 'Rebalancing Stategy',
-                    value='rebalance_strategy',
-                    children = [
-                    html.Div(rebalance_intro)])
+rebalance_strategy_tab =\
+dcc.Tab(
+    label = 'Rebalancing Stategy',
+    value='rebalance_strategy',
+    children = [
+                    html.Div('eyo'),
+                    dcc.Input()
+                ]
+)
