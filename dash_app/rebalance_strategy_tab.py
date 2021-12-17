f934805b3810cdc9b12a8a4987efd2227528bc13
charles phillips <charlesvphillips@gmail.com>
2021-12-17 15:37:04 -0500

    Gonna do something crazy

--- dash_app/rebalance_strategy_tab.py
+++ dash_app/rebalance_strategy_tab.py
@@ -1,5 +1,7 @@
 from dash import dcc, html
+import requests
 
+weather_dict =  request = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=40.7812&lon=-73.9665&exclude=minutely,current,daily,alerts&units=metric&appid=404310456b8e1c31228341dd6c95dd04')
 
 rebalance_strategy_tab =\
 dcc.Tab(
