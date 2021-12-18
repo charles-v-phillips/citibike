19858fbb1d83a4349696a3bafff2142c12e6340f
Jung Hwan Lim <junglim@Jungs-MBP-3.fios-router.home>
2021-12-17 21:16:32 -0500

    bonblusion tab

---
+++ dash_app/conclusion_tab.py
@@ -0,0 +1,5 @@
+from dash import dcc, html
+conclusion_tab = dcc.Tab(
+    label = 'Cuntclusion',
+    value = 'conclusion',
+    children = [html.Div('On top of the rebalancing that CitiBike already does, they still got more rebalancing to do smfh.')])
