f89039f82c21800554500af98d4901bd364c2428
Jung Hwan Lim <junglim@Jungs-MBP-3.fios-router.home>
2021-12-17 21:18:38 -0500

    buntblusion in layout

--- dash_app/layout.py
+++ dash_app/layout.py
@@ -4,6 +4,7 @@ import about_us_tab as about_us_tab
 import rebalancing_tab as rebalancing_tab
 import intro_tab as intro_tab
 import rebalance_strategy_tab as rebalance_strategy_tab
+import conclusion_tab as bonb_tab
 from PIL import Image
 
 citi_bike_logo = Image.open('./data/image/citi_bike_logo.png')
@@ -12,7 +13,8 @@ tabs = dcc.Tabs(id="tabs-example-graph", children=[intro_tab.intro_tab,
                                                     eda_tab.eda_tab,
                                                     rebalancing_tab.rebalancing_tab,
                                                     rebalance_strategy_tab.rebalance_strategy_tab,
-                                                    about_us_tab.about_us_tab],
+                                                    about_us_tab.about_us_tab
+                                                    bonb_tab.conclusion_tab],
                                                     value='intro')
 
 layout = html.Div([
